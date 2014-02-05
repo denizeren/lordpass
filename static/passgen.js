f09f209bffbea05f1f24df468c67dbe9475e228f
Deniz Eren <dee.116@gmail.com>
2014-02-05 20:33:57 +0200

    js fix

--- static/passgen.js
+++ static/passgen.js
@@ -1,4 +1,4 @@
 function passGen() {
-    document.getElementById('generatedpass').innerHTML = Sha256.hash(document.passgen.sword + document.passgen.site).substring(0,19);
+    document.getElementById('generatedpass').innerHTML = Sha256.hash(document.passgen.sword.value + document.passgen.site.value).substring(0,19);
     return false;
 }
