d2420048edab7256fe43ecabe492e9d61a297647
Deniz Eren <dee.116@gmail.com>
2014-02-05 21:09:01 +0200

    Pass size and versioning added

--- static/passgen.js
+++ static/passgen.js
@@ -1,4 +1,9 @@
 function passGen() {
-    document.getElementById('generatedpass').innerHTML = Sha256.hash(document.passgen.sword.value + document.passgen.site.value).substring(0,19);
+    var sizePass = document.passgen.sizepass.value;
+    var passVer = document.passgen.version.value;
+    if (sizePass == "") { sizePass = 64; }
+    if (passVer == "") { passVer = "1"; }
+    var generatedPass = Sha256.hash(document.passgen.sword.value + document.passgen.site.value + passVer).substring(0,sizePass);
+    document.getElementById('generatedpass').innerHTML = generatedPass;
     return false;
 }
