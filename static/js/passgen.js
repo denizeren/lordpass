4ab0878de178acac2b1c5c8a4dff0352eab5c274
Deniz Eren <dee.116@gmail.com>
2014-02-13 23:18:17 +0200

    Fix #1 Sha256 and bCrypt is now used for client side password generation

--- static/js/passgen.js
+++ static/js/passgen.js
@@ -1,18 +1,42 @@
-function passGen() {
-    var sizePass = $('#sizepass').val();
-    var passVer = $('#version').val();
+function result(hash){
+    var shaObj = new jsSHA(hash, "TEXT");
+    var genSha = shaObj.getHash("SHA-512", "HEX");
     var alphabet = $('#alphabet').val();
+    var sizePass = $('#sizepass').val();
 
-    if (sizePass == "") { sizePass = 15; }
-    if (passVer == "") { passVer = "1"; }
     if (alphabet == "") { alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"; }
+    if (sizePass == "") { sizePass = 15; }
 
-    var generatedPass = convert(Sha256.hash($('#sword').val() + $('#site').val() + passVer + $('#username').val()), alphabet).substring(0,sizePass);
+    var generatedPass = convert(genSha, alphabet).substring(0,sizePass);
 
     $('#generatedpass').val(generatedPass);
     $('#passalert').show();
     $('#generatedpass').focus();
     $('#generatedpass').select();
+}
+
+function crypt(sword, site, passVer, uname){
+    var bcrypt = new bCrypt();
+    var salt = site + passVer + uname;
+    var shaObj = new jsSHA(salt, "TEXT");
+
+    salt = "$2a$05$" + shaObj.getHash("SHA-512", "HEX").substring(0, 22)
+
+    try{
+        bcrypt.hashpw(sword, salt, result);
+    }catch(err){
+        alert(err);
+        return;
+    }
+}
+
+
+function passGen() {
+    var passVer = $('#version').val();
+
+    if (passVer == "") { passVer = "1"; }
+
+    crypt($('#sword').val(), $('#site').val(), passVer, $('#username').val());
 
     return false;
 }
